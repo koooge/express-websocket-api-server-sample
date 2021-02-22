5a8d67675a7fc4aa97f5bd6c931112ba51731a5a
koooge <koooooge@gmail.com>
2021-02-22 23:44:09 +0100

    feat: Hello ws
    Signed-off-by: koooge <koooooge@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # express-websocket-api-server-sample
 WebSocket Practice in Node.js
+
+
+- ws://localhost:3000/users
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const WebSocket = require('ws');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
